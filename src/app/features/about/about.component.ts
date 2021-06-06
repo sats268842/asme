a9e27845da72f933620bdb3bb374008aa6ce7a96
Santhosh Thomas <santhoshthomas@gmail.com>
2021-06-06 21:05:57 +0530

    initial

--- src/app/features/about/about.component.ts
+++ src/app/features/about/about.component.ts
@@ -1,3 +1,5 @@
+// @ts-ignore
+// @ts-nocheck
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,8 +10,34 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
 
   constructor() { }
-
+  speakers: number=0;
+  sponors: number=0;
+  events: number=0;
+  i=0;
+  interval;
   ngOnInit(): void {
+    
+    
   }
 
+ ngAfterViewInit(): void {
+   //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+   //Add 'implements AfterViewInit' to the class.
+  this.interval = setInterval(() => {
+    if(this.speakers<=3){
+      this.speakers++;
+     
+    }
+  if(this.sponors<=10){
+    this.sponors++;
+}
+if(this.events<=6){
+  this.events++;
+}
+if(this.sponors==11){
+  clearInterval(this.interval);
+}
+
+},300);
+ }
 }
