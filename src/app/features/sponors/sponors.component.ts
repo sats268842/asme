e7fa1d5c82b1aeb0db5df807c8e98f92b0fb9930
Santhosh Thomas <santhoshthomas@gmail.com>
2021-06-09 13:58:51 +0530

    initial

--- src/app/features/sponors/sponors.component.ts
+++ src/app/features/sponors/sponors.component.ts
@@ -14,9 +14,13 @@ export class SponorsComponent implements OnInit {
 
   logos=[
     'https://res.cloudinary.com/www-santhoshthomas-xyz/image/upload/v1623225051/asme/ASME_SSET_Chapter_holtts.jpg',
-    '../../../assets/SJCET TYPO PNG.png',
-    '../../../assets/SJCET TYPO PNG.png',
-    '../../../assets/SJCET TYPO PNG.png'
+    'https://res.cloudinary.com/www-santhoshthomas-xyz/image/upload/v1623225052/asme/IMG-20210607-WA0002_w8ljlr.jpg',
+    'https://res.cloudinary.com/www-santhoshthomas-xyz/image/upload/v1623225052/asme/ASME_MACE_Student_Section_logo_vm6jph.png',
+    'https://res.cloudinary.com/www-santhoshthomas-xyz/image/upload/v1623225052/asme/Rajagiri_School_of_Engineering_and_Technology_ui2gvk.png',
+    'https://res.cloudinary.com/www-santhoshthomas-xyz/image/upload/v1623225052/asme/ASME_SJCET_huxnxv.png',
+    'https://res.cloudinary.com/www-santhoshthomas-xyz/image/upload/v1623225052/asme/TKMCE_KOLLAM_z3ndr5.png',
+    'https://res.cloudinary.com/www-santhoshthomas-xyz/image/upload/v1623225053/asme/asme_mesce_pcxay5.png',
+    'https://res.cloudinary.com/www-santhoshthomas-xyz/image/upload/v1623225053/asme/GEC_Barton_Hill_hpibpl.jpg'
 
   ]
 
