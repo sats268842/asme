f0311cde75e22ad010f5e59f7715944780c6143b
Santhosh Thomas <santhoshthomas@gmail.com>
2021-06-12 22:46:35 +0530

    initial

--- src/app/features/program-schedule/program-schedule.component.ts
+++ src/app/features/program-schedule/program-schedule.component.ts
@@ -26,9 +26,17 @@ export class ProgramScheduleComponent implements OnInit {
 
     },
     {
+      'title': 'E-Talk',
+      'subtitle': 'Vinay Menon',
+      'time': '7.00 PM - 7.30 PM',
+      'details': '',
+      'register': false,
+
+    },
+    {
       'title': 'Bodhi',
       'subtitle': 'Interactive session with Mr.Maithreyan',
-      'time': '7.00 PM - 8.00 PM',
+      'time': '7.45 PM - 8.45 PM',
       'details': "\"Your heart is where your treasure is, and you must find your treasure in order to make sense of everything.<br />   Paulo Coelho\n\nHere comes ASME EFx with an electrifying virtual treasure hunt event. \nThrough an  exciting digital puzzles the participants are going to sharpen the sequential thinking \nskills  about the current pandemic scenario.\n\n 🔰 Prizes worth RS 4000 /-♨️\n 🔰 Individual participation \n 🔰The event will be hosted through website.\n 🔰Will go live by June 12, Saturday morning and close by June 13, Sunday noon. \n\n In this digital treasure hunt we will be following up a storyline.\nAnd the theme goes like:\n\nThe COVID-19 pandemic has evolved into a deadlier disease. Once infected the virus will be eradicated in about two weeks, but most fail to make it and depart this life.\nThe govt has issued strict lockdowns due to the massive increase in death rates which brought the economic life stand still.\nRiots break out in the streets, govt is unable to exercise control over people, cities are ruled by gangs and the like which makes the situation worse.\n Its time for a superhero . \nAmid the ongoing pandemic, A graduate mechanical engineer wakes up. A crowned one man army to fight against the pandemic with his greatest powers of knowledge and skills to bring back to normalcy. A combat between man of courage and pandemic . A  combat for the existence of man kind.\n\n Will our superhero be the saviour❗\n Soldier are u ready ⁉️\n SUIT UP ‼️",
       'register': true,
     }
@@ -80,21 +88,7 @@ export class ProgramScheduleComponent implements OnInit {
   ]
 
   day3 = [
-    {
-      'title': 'PRIDE AND UNPREJUDICED',
-      'subtitle': 'Queerhythm',
-      'time': '9.00 AM - 9.30 AM',
-      'details': '',
-      'register': true,
-
-    },
-    {
-      'title': 'SPIRITED SYMPHONY',
-      'subtitle': 'Jim Jacob (Four Music)',
-      'time': '10.00 AM - 10.30 AM',
-      'details': '',
-      'register': true
-    },
+   
     {
       'title': ' G.O.A.T',
       'subtitle': 'Round 3 - Interview',
@@ -109,6 +103,13 @@ export class ProgramScheduleComponent implements OnInit {
       'details': '',
       'register': true,
 
+    }, 
+    {
+      'title': 'SPIRITED SYMPHONY',
+      'subtitle': 'Jim Jacob (4 Musics)',
+      'time': '11.00 AM - 12.00 PM',
+      'details': '',
+      'register': true
     },
     {
       'title': 'Digital Holmes',
@@ -116,6 +117,14 @@ export class ProgramScheduleComponent implements OnInit {
       'time': '12.00 PM',
       'details': '',
       'register': false,
+
+    },
+    {
+      'title': 'PRIDE AND UNPREJUDICED',
+      'subtitle': 'Queerhythm',
+      'time': '2.30 PM - 3.30 PM',
+      'details': '',
+      'register': true,
 
     },
     {
