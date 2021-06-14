73c51ed9ee372db4d75253b5b0203bb41e5c505a
Santhosh Thomas <santhoshthomas@gmail.com>
2021-06-14 14:11:56 +0530

    server

--- src/app/features/sponors/sponors.component.ts
+++ src/app/features/sponors/sponors.component.ts
@@ -19,7 +19,7 @@ export class SponorsComponent implements OnInit {
     'https://res.cloudinary.com/www-santhoshthomas-xyz/image/upload/v1623225052/asme/Rajagiri_School_of_Engineering_and_Technology_ui2gvk.png',
     'https://res.cloudinary.com/www-santhoshthomas-xyz/image/upload/v1623658969/asme/SJCET_vcivjs.png',
     'https://res.cloudinary.com/www-santhoshthomas-xyz/image/upload/v1623225052/asme/TKMCE_KOLLAM_z3ndr5.png',
-    'https://res.cloudinary.com/www-santhoshthomas-xyz/image/upload/v1623225053/asme/asme_mesce_pcxay5.png',
+    'https://res.cloudinary.com/www-santhoshthomas-xyz/image/upload/v1623659501/asme/MESCE_jdczhq.png',
     'https://res.cloudinary.com/www-santhoshthomas-xyz/image/upload/v1623225053/asme/GEC_Barton_Hill_hpibpl.jpg',
     'https://res.cloudinary.com/www-santhoshthomas-xyz/image/upload/v1623438843/asme/ASME_FISAT_LOGO_OFFICIAL_1_qc25lf.png',
     'https://res.cloudinary.com/www-santhoshthomas-xyz/image/upload/v1623658969/asme/illah_ewez7j.jpg'
