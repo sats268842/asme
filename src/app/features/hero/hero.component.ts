7d3a9c80e8f4ba985d248744b0bbace6f784d060
Santhosh <santhoshthomas@gmail.com>
2021-06-18 01:17:21 +0530

    Online

--- src/app/features/hero/hero.component.ts
+++ src/app/features/hero/hero.component.ts
@@ -41,10 +41,10 @@ export class HeroComponent implements OnInit {
     setInterval(() => {
       this.date1 = new Date();
       this.date2 = new Date('06/18/2021');
-      this.Difference_In_Time =(  this.date1.getTime()- this.date2.getTime() );
+      this.Difference_In_Time =(  this.date2.getTime()-this.date1.getTime() );
       // console.log('count')
       this.diffInDays = Math.ceil(this.Difference_In_Time / (1000 * 3600 * 24));
-      this.hour =-(Math.floor(
+      this.hour =16-(Math.floor(
         (this.Difference_In_Time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
       );
       this.minutes =-(Math.floor(
